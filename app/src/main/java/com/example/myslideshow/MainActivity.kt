db8eb5b848d52624b585c6f8ef11211ae402e65e
nargok <nargok@gmail.com>
2019-05-18 11:17:02 +0900

    5秒ごとに写真をスライドする

--- app/src/main/java/com/example/myslideshow/MainActivity.kt
+++ app/src/main/java/com/example/myslideshow/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myslideshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,5 +41,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pager.adapter = MyAdapter(supportFragmentManager)
+
+
+        // タイマー処理を追加して、5秒ごとにスライドおくりする
+        // Handlerクラスを使って別スレッド処理を実現する
+        // 別スレッドで実現したい処理をpostメソッドの中に実装する
+        val handler = Handler()
+        timer(period = 5000) {
+            handler.post {
+                pager.currentItem = (pager.currentItem + 1) % 10
+            }
+        }
     }
 }
